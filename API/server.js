ab9b4cf71d24a89a9d4520d77930ec5567fbb16b
WernkeJD <Jacob.Wernke@yahoo.com>
2024-08-23 23:44:34 -0400

    moved manifest.json to root and updated premissions + files to fit google requirements

---
+++ API/server.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import pkg from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+const { json } = pkg;
+
+
+const app = express();
+app.use(json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+const EDAMAM_APP_ID = process.env.EDAMAM_APP_ID
+const EDAMAM_API_KEY = process.env.EDAMAM_API_KEY
+
+console.log(OPENAI_API_KEY)
+
+app.post('/getNutritionData', async (req, res) => {
+    const { text } = req.body
+
+    try {
+        const gptResponse = await fetch("https://api.openai.com/v1/chat/completions", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${OPENAI_API_KEY}`,
+          },
+          body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            response_format: {"type": "json_object"},
+            messages: [
+              {
+                role: "user",
+                content: text + "Can you list the ingredients with the quantities for me and only print the answer? Extract the Items, Encapsulate Each Item in Quotes: Surround each item with double quotes. This denotes each item as a string. Separate Items with Commas: Place a comma between each item to separate them within the list. Enclose in Square Brackets: Place the entire list of items inside square brackets to define it as an array. Basically, return json which has each ingredient with its quanteities always use the key Ingredients with a capital I",
+              },
+            ],
+          }),
+        });
+        const gptData = await gptResponse.json();
+        console.log(gptData)
+        // res.status(200).send(gptData);
+        const ingredients = gptData.choices[0].message.content;
+        console.log(ingredients);
+
+        let ingredients_list = [];
+
+        ingredients_list = JSON.parse(ingredients).Ingredients
+      //   Object.entries(JSON.parse(ingredients)).forEach(([key, value]) => {
+      //     console.log(`${key}: ${value}`);
+      //     ingredients_list.push(`${key}: ${value}`);
+      //     console.log(ingredients_list);
+      // });
+    
+        const edamamResponse = await fetch(`https://api.edamam.com/api/nutrition-details?app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_API_KEY}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ingr: ingredients_list }),
+        });
+        const nutritionData = await edamamResponse.json();
+    
+        const calories = nutritionData.totalNutrients.ENERC_KCAL.quantity.toFixed(2);
+        const protein = nutritionData.totalNutrients.PROCNT.quantity.toFixed(2);
+        const fat = nutritionData.totalNutrients.FAT.quantity.toFixed(2);
+        const carbs = nutritionData.totalNutrients.CHOCDF.quantity.toFixed(2);
+    
+        res.json({ calories, protein, fat, carbs });
+        
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send(error);
+    }
+});
+    
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- Frontend/popup.js
+++ Frontend/popup.js
@@ -1,17 +1,68 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const fetchContentButton = document.getElementById('fetchContent');
-    console.log('Popup script loaded, adding event listener to button');
+document.addEventListener("DOMContentLoaded", function () {
+  const fetchContentButton = document.getElementById("fetchContent");
+  console.log("Popup script loaded, adding event listener to button");
 
-    if (fetchContentButton) {
-        fetchContentButton.addEventListener('click', () => {
-            console.log('Fetch Content button clicked, sending message to background script');
-            chrome.runtime.sendMessage({ action: 'fetchContent' }, function(response) {
-                console.log('Response from background script:', response);
-            });
-        });
-    } else {
-        console.error('Fetch Content button not found');
-    }
+
+  if (fetchContentButton) {
+    fetchContentButton.addEventListener("click", () => {
+      console.log("Fetch Content button clicked");
+  
+      showSpinner();
+  
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tabs[0].id },
+            function: getInnerText,
+          },
+          async (results) => {
+            const text = results[0].result;
+  
+            try {
+              const response = await fetch('https://calorie-counter-k4kc.onrender.com/getNutritionData', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ text: text }),
+              });
+  
+              const data = await response.json();
+  
+              const { calories, protein, fat, carbs } = data;
+  
+              document.getElementById("content").innerText = 
+              `Calories: ${calories} kcal\n` +
+              `Protein: ${protein} g\n` +
+              `Fat: ${fat} g\n` +
+              `Carbs: ${carbs} g`;
+  
+              hideSpinner();
+            } catch (error) {
+              console.error("Error:", error);
+              hideSpinner();
+            }
+          }
+        );
+      });
+    });
+  } else {
+    console.error("Fetch Content button not found");
+  }
+  
+  function getInnerText() {
+    return document.body.innerText;
+  }
+  
+  function showSpinner() {
+    console.log('show spinner called');
+    document.getElementById('loading-spinner').style.display = 'block';
+    document.getElementById('initial-content').style.display = 'none';
+  }
+  
+  function hideSpinner() {
+    console.log('hide spinner called');
+    document.getElementById('loading-spinner').style.display = 'none';
+    document.getElementById('initial-content').style.display = 'block';
+  }
 });
-
-
